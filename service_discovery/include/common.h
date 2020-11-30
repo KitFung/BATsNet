6d51f5fbbbc768af682c6faa81848db3f6352ff5
Kit Fung <sa9510@gmail.com>
2020-11-30 17:29:51 +0800

    Fix the temp var

--- service_discovery/include/common.h
+++ service_discovery/include/common.h
@@ -2,12 +2,12 @@
 
 namespace service_discovery {
 
-// const char kBATs_ip[] = "10.0.0.100";
-const char kBATs_ip[] = "10.42.0.100";
+const char kBATs_ip[] = "10.0.0.100";
+// const char kBATs_ip[] = "10.42.0.100";
 const int kBATs_qport = 3314;
 
-// const char ketcd_src[] = "10.0.0.100:2379";
-const char ketcd_src[] = "http://10.42.0.100:2379";
+const char ketcd_src[] = "10.0.0.100:2379";
+// const char ketcd_src[] = "http://10.42.0.100:2379";
 const char klocal_etcd_src[] = "http://127.0.0.1:2379";
 
 } // namespace service_discovery
