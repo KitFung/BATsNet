befcaee44a11b690fbdc3975c801c87cf757a4a0
Kit Fung <sa9510@gmail.com>
2020-12-04 17:20:21 +0800

    Add the test radar manual cfg script

--- radar_iwr6843/debug/test.py
+++ radar_iwr6843/debug/test.py
@@ -23,16 +23,24 @@ def run():
     state.base.mode = common_pb2.BasicMutableState.Mode.Value('SHARING')
     res = stub.SetState(state)
     print(res)
+
     print("-----------LocalSaving----------")
     state.base.mode = common_pb2.BasicMutableState.Mode.Value('LOCAL_SAVING')
     res = stub.SetState(state)
     print(res)
+
     print("-----------Change Mode----------")
     state.base.mode = common_pb2.BasicMutableState.Mode.Value('SHARING')
     state.current_mode = "OUTDOOR"
     res = stub.SetState(state)
     print(res)
 
+    print("-----------Write Manual Cfg-----------")
+    manual_cfg = "sensorStop\nflushCfg\nsensorStart\n"
+    cfg_object = radar_pb2.LadarCfg()
+    cfg_object.data = manual_cfg
+    res = stub.SetManualCfg(cfg_object)
+    print(res)
 
 if __name__ == '__main__':
     run()
