95efb47c1488f9cf45304c53f8c6839dbc238eaa
Kit Fung <sa9510@gmail.com>
2021-01-28 14:22:41 +0800

    Update cron install

--- fog_side/data_sync/install_cron.sh
+++ fog_side/data_sync/install_cron.sh
@@ -1,4 +1,4 @@
 cp data_sync /etc/cron.d/data_sync
 mkdir -p /opt/aiot/data_sync/
 cp simple_rsync_upload.sh /opt/aiot/data_sync/
-crontab -u nvidia /etc/cron.d/data_sync
+sudo crontab -u root /etc/cron.d/data_sync
